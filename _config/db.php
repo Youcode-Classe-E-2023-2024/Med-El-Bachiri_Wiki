cd4c528993f36f0621f65e9ee08d72752334050c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-08 20:20:43 +0100

    connecting to database

--- _config/db.php
+++ _config/db.php
@@ -1,1 +1,10 @@
 <?php
+
+try {
+    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
