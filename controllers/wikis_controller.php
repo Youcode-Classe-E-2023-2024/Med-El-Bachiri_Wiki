0f087cdf023372a5bb4bc895bca3b8de11794158
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-09 18:32:18 +0100

    get all categories to display when add wiki

--- controllers/wikis_controller.php
+++ controllers/wikis_controller.php
@@ -3,3 +3,16 @@
 if (!isset($_SESSION['user_id'])) {
     header('location: index.php?page=login');
 }
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (isset($data['type']) && !empty($data['type'] && $data['type'] === 'getCat')) {
+        $result = Category::getAll();
+        if ($result) {
+            exit(json_encode($result));
+        }
+        exit();
+    }
+}
