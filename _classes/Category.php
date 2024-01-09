1a86ddc54b4eb79f2e52b264dcd5572471b64372
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-09 09:19:14 +0100

    add construct function | get category by id

--- _classes/Category.php
+++ _classes/Category.php
@@ -4,4 +4,20 @@ class Category
 {
     public $id;
     public $name;
+
+    public function __construct($id)
+    {
+        global $db;
+
+        $query = "SELECT * FROM categories WHERE id = :id";
+        $stm = $db->prepare($query);
+        $stm->bindValue(':id', $id, PDO::PARAM_INT);
+        $stm->execute();
+        $user = $stm->fetch(PDO::FETCH_ASSOC);
+
+        if ($user !== false > 0) {
+            $this->id = $user['id'];
+            $this->name = $user['name'];
+        }
+    }
 }
