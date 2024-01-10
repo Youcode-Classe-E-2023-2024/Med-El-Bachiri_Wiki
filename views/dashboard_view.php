79042c43975c60002dc1e94a360eaaee029e9542
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-10 09:25:40 +0100

    update dashboard

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -56,6 +56,23 @@
                             xmlns="http://www.w3.org/2000/svg"
                     >
                         <path
+                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                        ></path>
+                    </svg>
+                    <span class="">Categories</span>
+                </div>
+
+                <div
+                        onclick="hideDivs(1)"
+                        class="btnDiv text-sm cursor-pointer font-medium text-gray-700 py-2 px-2 rounded-md transition duration-150 ease-in-out"
+                >
+                    <svg
+                            class="w-6 h-6 fill-current inline-block"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
                                 fill-rule="evenodd"
                                 d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                                 clip-rule="evenodd"
@@ -65,42 +82,8 @@
                 </div>
 
                 <div
-                        href=""
-                        class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-purple-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-                >
-                    <svg
-                            class="w-6 h-6 fill-current inline-block"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                                d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
-                        ></path>
-                    </svg>
-                    <span class="">Users</span>
-                </div>
-
-                <div
-                        onclick=""
-                        href=""
-                        class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-purple-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
-                >
-                    <svg
-                            class="w-6 h-6 fill-current inline-block"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                        ></path>
-                    </svg>
-                    <span class="">Categories</span>
-                </div>
-                <div
-                        href=""
-                        class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-purple-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                        onclick="hideDivs(2)"
+                        class="btnDiv text-sm font-medium text-gray-700 py-2 px-2 cursor-pointer rounded-md transition duration-150 ease-in-out"
                 >
                     <svg
                             class="w-6 h-6 fill-current inline-block"
@@ -114,6 +97,7 @@
                     </svg>
                     <span class="">Wikis</span>
                 </div>
+                
 
             </div>
         </div>
