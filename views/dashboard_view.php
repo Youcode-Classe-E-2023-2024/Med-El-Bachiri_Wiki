fdebadbe749e9bcba816b3554eaced84d208608f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-10 09:25:11 +0100

    show cat div when click

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -45,11 +45,9 @@
             </div>
 
             <div id="menu" class="flex flex-col space-y-2">
-
                 <div
-                        onclick="showCatDiv()"
-                        href=""
-                        class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-purple-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+                        onclick="hideDivs(0)"
+                        class="btnDiv text-sm cursor-pointer font-medium text-gray-700 py-2 px-2 hover:text-base rounded-md transition duration-150 ease-in-out"
                 >
                     <svg
                             class="w-6 h-6 fill-current inline-block"
