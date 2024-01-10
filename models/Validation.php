41f1e4ec67185d38aaf4635573effccdb9f2490c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-10 14:48:03 +0100

    validation class

---
+++ models/Validation.php
@@ -0,0 +1,48 @@
+<?php
+
+class Validation
+{
+
+    static function validateUsername($username)
+    {
+        if (empty($username)) {
+            return "Username is required";
+        } elseif (!preg_match('/^[a-zA-Z0-9]{3,}$/', $username)) {
+            return "Invalid username. Username should be at least 3 characters long.";
+        }
+        return false;
+    }
+
+    /**
+     * @throws Exception
+     */
+    static function userChecker($email)
+    {
+        if (User::checkIfUserExist($email)) {
+            return "User already exists";
+        }
+        return true;
+    }
+
+
+    static function validateEmail($email)
+    {
+        if (empty($email)) {
+            return "Email is required";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return "Invalid email format.";
+        }
+        return false;
+    }
+
+    static function validatePassword($password)
+    {
+        if (empty($password)) {
+            return "Password is required";
+        } elseif (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', $password)) {
+            return "Invalid password. Password should have at least 8 characters, including one uppercase letter, one lowercase letter, and one number.";
+        }
+        return false;
+    }
+
+}
