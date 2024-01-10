26a7f8430827b9e9bc1c58e095bf86fd7cc26754
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-10 09:19:21 +0100

    pagination in dashboard

--- assets/js/DOM.js
+++ assets/js/DOM.js
@@ -1,1 +1,18 @@
+const btnDiv = document.querySelectorAll('.btnDiv');
+let divs = document.querySelectorAll('.divs');
 
+// hide all divs except the param of the function
+function hideDivs(except) {
+    divs.forEach((div, i) => {
+        div.style.display = 'none';
+        btnDiv[i].style.backgroundColor = 'white';
+    });
+    divs[except].style.display = 'block';
+    btnDiv[except].style.backgroundColor = 'rgba(83,30,238,0.73)';
+    if (except === 1) {
+        getAllTagsForAdmin();
+    }
+}
+
+// hide all divs except the first div by default
+hideDivs(0);
