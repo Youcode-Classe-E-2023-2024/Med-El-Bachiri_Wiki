07a177ad3d69a1c8f548210431901f6ed6277caa
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 22:47:36 +0100

    take add wiki js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -380,52 +380,6 @@ function editTag(tagId) {
 }
 //
 
-// add wiki
-let submitWiki = document.querySelector('#submitWiki');
-const tags = document.querySelector('.selectedTags');
-
-submitWiki.addEventListener('click', function (event) {
-    let selectedTags = [];
-    const options = tags.selectedOptions;
-    for (let i = 0; i < options.length; i++) {
-        selectedTags.push(options[i].value);
-    }
-
-    let title = document.querySelector('#wikiTitle');
-    let content = document.querySelector('#wikiContent');
-    let idCategory = document.querySelector('#wiki_id_category');
-
-    if (title.value !== '' && content.value !== '' && idCategory.value !== '' && selectedTags.length !== 0) {
-        let dataToSend = {
-            type: 'addWiki',
-            title: title.value,
-            content: content.value,
-            id_category: idCategory.value,
-            tags: selectedTags
-        };
-
-        fetch('index.php?page=wikis', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(dataToSend),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                title.value = '';
-                content.value = '';
-                alert('WIKI ADDED!');
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    } else {
-        alert('MUST FILL ALL INPUTS');
-    }
-});
-//
 
 
 // get all tags for author
@@ -447,7 +401,7 @@ function getAllTagsForAuthor() {
         .then(data => {
             data.forEach(tag => {
                 tagsGoHereAuthor.innerHTML += `
-                <option value="${tag.id}" class="flex rounded-sm justify-center items-center border">
+                <option value="${tag.id}" class="flex rounded-sm justify-center items-center border text-black">
                     ${tag.name}
                 </option>
                 `;
