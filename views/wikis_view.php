b35ab2073806794e47c669e0fb1ba16ef020327d
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 23:01:24 +0100

    Update wikis_view.php

--- views/wikis_view.php
+++ views/wikis_view.php
@@ -1,74 +1,56 @@
-<button onclick="addWikiAlert()" class="bg-purple-600 m-2 hover:opacity-80 shadow-xl rounded-full py-2 px-4 text-white">Add Wiki</button>
+<p onclick="addWikiShowFrom()" class="bg-purple-600 m-2 w-32 flex justify-center hover:opacity-80 shadow-xl border-black border cursor-pointer rounded-full py-2 px-4 text-white">Add Wiki</p>
 
-<div value="${ticket.ticketId}" class="ticketDiv bg-white max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm border mb-4">
-    <div class="flex justify-between">
-        <div class="usersAndProfiles flex bg-gray-100 rounded-xl p-2">
-            <!-- User names and profiles will be displayed here -->
-        </div>
-        <div class="flex items-end">
-            <p rel="noopener noreferrer" class="mt-2 mr-4 opacity-70 text-center  bg-gradient-to-l text-black p-2 from-gray-400 font-medium rounded bg-white">${ticket.status}</p>
-            <p rel="noopener noreferrer" class="bg-gradient-to-l from-purple-700  p-2 font-bold rounded bg-blue-700 text-white">${ticket.priority}</p>
-        </div>
+<div id="wikiBody">
+
+</div>
+
+<div id="addWikiForm" class="absolute shadow-xl bg-gray-800 text-white" style="top: 15%; right: 36%; width: 400px; padding: 20px; border-radius: 10px; display: none;">
+    <button class="float-right text-gray-400 hover:text-gray-200" onclick="closeWikiForm()">X</button>
+    <h2 class="text-2xl font-bold mb-4">Create a Wiki</h2>
+
+    <div class="mb-4">
+        <label for="wikiTitle" class="block text-gray-400">Title</label>
+        <input type="text" id="wikiTitle" placeholder="" class="text-black px-3 py-1 form-input mt-1 block w-full rounded-md">
     </div>
-    <div class="flex items-center justify-between">
-        <p class="hover:underline text-green-500 text-sm text-gray-700 my-2">#${ticket.tag}</p>
+
+    <div class="mb-4">
+        <label for="wikiContent" class="block text-gray-400">Description</label>
+        <textarea rows="4" id="wikiContent" placeholder="" class="form-input mt-1 text-black px-3 py-1 block w-full rounded-md"></textarea>
     </div>
-    <div class="mt-3">
-        <a rel="noopener noreferrer" href="#" class="text-black text-2xl font-bold hover:underline">${ticket.title}</a>
-        <p class="text-black mt-2">${ticket.description}</p>
-    </div>
-    <div class="flex items-center justify-between m-4">
-        <form action="pages/ticketDetail.php" method="post">
-            <button rel="readMreBtn noopener noreferrer" class="hover:underline text-blue-400">Read more</button>
-            <input name="ticketId" type="hidden" value="${ticket.ticketId}">
-            <input name="userId" type="hidden" value="${ticket.userId}">
-        </form>
+
+    <div class="grid grid-cols-2 gap-4 my-4">
         <div>
-            <a rel="noopener noreferrer" href="#" class="flex items-center">
-                <p class="text-sm text-gray-600">Created By</p>
-                <img src="img/${ticket.imagePath}" alt="avatar" class="object-cover w-10 h-10 mx-4 rounded-full bg-gray-500">
-                <p class="text-black">${ticket.username}</p>
-            </a>
+            <label for="category" class="block text-gray-400">
+                Category
+                <select id="wiki_id_category" class="categoriesGoHere border-none form-select mt-1 block w-full rounded-md text-gray-800">
+                    <!-- display categories here -->
+                </select>
+            </label>
         </div>
     </div>
-</div>
 
-
-<div>
-    <div class="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
-        <h2 class="text-2xl font-bold mb-4">Create a Wiki</h2>
-
-        <div class="mb-4">
-            <label for="wikiTitle" class="block text-gray-600">Title</label>
-            <input type="text" id="wikiTitle" placeholder="" class="border form-input mt-1 block w-full rounded-md">
-        </div>
-
-        <div class="mb-4">
-            <label for="wikiContent" class="block text-gray-600">Description</label>
-            <textarea rows="4" id="wikiContent" placeholder="" class="border form-input mt-1 block w-full rounded-md"></textarea>
-        </div>
-
-        <div class="grid grid-cols-2 gap-4 border my-4">
-
-            <div>
-                <label for="category" class="block text-gray-600">
-                    Category
-
-                    <select id="wiki_id_category" class="categoriesGoHere border form-select mt-1 block w-full rounded-md">
-                        <!-- display categories here -->
-                    </select>
-                </label>
-            </div>
-        </div>
-
-        <div class="my-4 border">
-            <label for="tagsGoHereAuthor" class="block text-gray-600">Choose Tags</label>
-            <select id="tagsGoHereAuthor" class="selectedTags form-multiselect mt-1 block w-full rounded-md" multiple>
-                <!-- display tags here  -->
-            </select>
-        </div>
-
-        <button id="submitWiki" type="button" onclick="" class="bg-purple-700 w-full text-white py-2 px-4 rounded-md hover:bg-purple-600">Submit</button>
+    <div class="my-4">
+        <label for="tagsGoHereAuthor" class="block text-gray-400">Choose Tags</label>
+        <select id="tagsGoHereAuthor" class="selectedTags text-black form-multiselect mt-1 block w-full rounded-md" multiple>
+            <!-- display tags here  -->
+        </select>
     </div>
 
+    <button id="submitWiki" type="button" onclick="" class="bg-purple-700 w-full text-white py-2 px-4 rounded-md hover:bg-purple-600">Submit</button>
+    <button id="editWikiSubmit" style="display: none;" type="button" onclick="saveWikiEdit()" class="bg-yellow-500 w-full text-white py-2 px-4 rounded-md hover:bg-purple-600">Save</button>
+    <input type="hidden" id="currentWikiID">
+
 </div>
+
+<script>
+    // add wiki form dom
+    const addWikiForm = document.querySelector('#addWikiForm');
+
+    function addWikiShowFrom() {
+        addWikiForm.style.display = (addWikiForm.style.display === 'block') ? 'none' : 'block';
+    }
+
+    function closeWikiForm() {
+        addWikiForm.style.display = 'none';
+    }
+</script>
