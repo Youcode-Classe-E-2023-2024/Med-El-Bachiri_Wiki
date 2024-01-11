8ff21d50a33316efa875c6719cba272dfe5275e6
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 14:48:49 +0100

    get the request & check data and return response

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -6,23 +6,30 @@ if (isset($_SESSION['user_id'])) {
 
 
 // log in
-if(isset($_POST['login_btn'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    $email = $data['email'];
+    $password = $data['password'];
+
+
     try {
         $user = User::login($email, $password);
+        if ($user === false) {
+            echo json_encode(['success' => false, 'message' => 'User not found']);
+        } elseif ($user === "passError") {
+            echo json_encode(['success' => false, 'message' => 'Password incorrect']);
+        } else {
+            echo json_encode(['success' => true]);
+            $_SESSION['user_id'] = $user['user_id'];
+        }
+        exit;
     } catch (Exception $e) {
-        die($e->getMessage());
-    }
-    if ($user !== false){
-        header('location: index.php?page=home');
-        $_SESSION['user_id'] = $user['user_id'];
-        $_SESSION['role'] = $user['role'];
-    } else {
-        header('location: index.php?page=login');
+        echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+        exit;
     }
 }
-
 
 // log out
 if (isset($_SESSION['user_id']) && isset($_POST['logout_btn'])) {
