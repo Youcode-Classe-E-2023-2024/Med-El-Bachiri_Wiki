94e7b88fa6a101dfb73a1a620b9bde233e68dd7c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 01:06:21 +0100

    get all cat function for admin

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,4 +228,74 @@ function addTag() {
         alert('MUST FILL THE INPUT');
     }
 }
+
+// get all tags for admin
+function getAllTagsForAdmin() {
+    const tagsGoHere = document.querySelector('#tagsGoHere');
+    if (tagsGoHere !== null) {
+        tagsGoHere.innerHTML = '';
+    }
+    fetch('index.php?page=dashboard', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ type: 'getTags' }),
+    })
+        .then(response => {
+            console.log(response.status);
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(tag => {
+                tagsGoHere.innerHTML += `
+                    <tr class="hover:bg-gray-50">
+
+            <td id="currentTagName-${tag.id}" class="px-6 py-4 text-purple-600 w-full">${tag.name}</td>
+
+            <td class="px-6 py-4">
+                <div class="flex justify-end gap-4">
+                    <button onclick="deleteTag(${tag.id})" class="hover:bg-gray-300 rounded-full p-2">
+                        <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="h-6 w-6"
+                                x-tooltip="tooltip"
+                        >
+                            <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                        </svg>
+                    </button>
+                    <button onclick="editTagAlert(${tag.id})" class="hover:bg-gray-300 rounded-full p-2">
+                        <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="h-6 w-6"
+                                x-tooltip="tooltip"
+                        >
+                            <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </td>
+        </tr>
+                `;
+            });
+        })
+        .catch(error => console.log(error));
+}
+//
 }
