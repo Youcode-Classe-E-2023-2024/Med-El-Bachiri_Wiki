e4fd3021ddfafc9e2d749e80c6ecb90b1e10e96c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 01:10:05 +0100

    update register controller | ajax

--- controllers/register_controller.php
+++ controllers/register_controller.php
@@ -5,19 +5,37 @@ if (isset($_SESSION['user_id'])) {
     header('location: index.php?page=home');
 }
 
-
 // register
-if (isset($_POST['register_btn'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    try {
-        $user = User::register($username, $email, $password);
-        if ($user){
-            header('location: index.php?page=login');
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    $username = $data['username'];
+    $email = $data['email'];
+    $password = $data['password'];
+
+
+    $errors = [
+        "username_err" => Validation::validateUsername($username),
+        "userExist_err" => Validation::userChecker($email),
+        "email_err" => Validation::validateEmail($email),
+        "password_err" => Validation::validatePassword($password),
+    ];
+
+
+    if (array_filter($errors)) {
+
+        echo json_encode(["errors" => $errors]);
+        exit;
+    } else
+        try {
+            $user = User::register($username, $email, $password);
+            if ($user) {
+                echo json_encode(['success' => true, 'message' => 'Registration successful']);
+                exit;
+            }
+        } catch (Exception $e) {
+            echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+            exit;
         }
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
 }
