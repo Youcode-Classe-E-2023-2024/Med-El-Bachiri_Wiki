ca62eb1760686a50410b1296162ebd77d086136a
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 01:11:34 +0100

    show wiki or dashboard button | its up to the user role

--- views/_layout.php
+++ views/_layout.php
@@ -34,8 +34,12 @@
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="index.html" class="bg-gray-900 hover:bg-gray-800 m-2 text-white rounded-md px-3 py-2 text-sm" aria-current="page">Home</a>
-                                <a href="" class="text-gray-300 m-2 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                <a href="index.php?page=home" class="bg-gray-900 hover:bg-gray-800 m-2 text-white rounded-md px-3 py-2 text-sm" aria-current="page">Home</a>
+                                <?php if (isset($user->role) && $user->role === 'admin') { ?>
+                                <a href="index.php?page=dashboard" class="text-gray-300 m-2 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+                                <?php } elseif (isset($user->role) && $user->role === 'author') { ?>
+                                <a href="index.php?page=wikis" class="text-gray-300 m-2 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Wikis</a>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
