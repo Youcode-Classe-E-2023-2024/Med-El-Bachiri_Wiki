04266e6ae0739563fad7134b4bde482d39c15c20
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 23:03:38 +0100

    edit wiki handler

--- controllers/wikis_controller.php
+++ controllers/wikis_controller.php
@@ -48,6 +48,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
         exit();
     }
+
+    if (isset($data['type']) && !empty($data['type']) && $data['type'] === 'editWiki') {
+        // Update the wiki
+        Wiki::update($data['title'], $data['content'], $data['id_category'], $data['wiki_id'], $_SESSION['user_id']);
+
+        // Delete existing tags for the wiki
+        WikiTag::deleteWikiTags($data['wiki_id']);
+
+        // Add the new tags for the wiki
+        foreach ($data['tags'] as $tag) {
+            WikiTag::add($data['wiki_id'], $tag);
+        }
+
+        exit; // Make sure this exit is used for your specific use case
+    }
+
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
     $data = json_decode(file_get_contents('php://input'), true);
     if (isset($data['type']) && !empty($data['type']) && $data['type'] === 'deleteWiki') {
