9920dd4d0b52d301a70db91d659a23f0f19cfc7e
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-11 01:10:13 +0100

    Update wikis_controller.php

--- controllers/wikis_controller.php
+++ controllers/wikis_controller.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user_id'])) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
 
-    if (isset($data['type']) && !empty($data['type'] && $data['type'] === 'getCat')) {
+    if (isset($data['type']) && !empty($data['type']) && $data['type'] === 'getCat') {
         $result = Category::getAll();
         if ($result) {
             exit(json_encode($result));
