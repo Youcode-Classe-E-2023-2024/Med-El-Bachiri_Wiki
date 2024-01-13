1d813ef2e4feb1bffd815a0d64e49c67ff8426c5
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-13 20:31:13 +0100

    SearchByTag method => return wikis ids

--- models/Search.php
+++ models/Search.php
@@ -16,4 +16,22 @@ class Search
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+
+    static function searchByTag($name)
+    {
+        global $db;
+        $tag_name = '%' . $name . '%';
+        $sql = "
+           SELECT w.id
+            FROM wikis w
+                     join wikis_tags wt on w.id = wt.id_wiki
+                     join tags t on t.id = wt.id_tag
+            WHERE t.name LIKE :tag_name
+              AND status = 'published';
+            ";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':tag_name', $tag_name, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
