a72cc8977600244b757f1ea23a0e4d30c8eacfa6
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-13 20:27:12 +0100

    search by title handler

--- controllers/all-wikis_controller.php
+++ controllers/all-wikis_controller.php
@@ -1,1 +1,18 @@
 <?php
+
+if (isset($_GET['search_by_title'])) {
+    $input = $_GET['search_by_title'];
+    $searchedWikisIDs = Search::searchByTitle($input);
+    $output = [];
+    foreach ($searchedWikisIDs as $wiki) {
+        try {
+            $wikiDetail = Wiki::getWiki($wiki['id']);
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $output[] = $wikiDetail;
+    }
+    echo json_encode($output);
+    exit;
+}
