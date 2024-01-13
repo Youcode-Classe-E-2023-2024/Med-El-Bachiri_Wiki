f72d6161f548f575e7e8202267d1eb4866aa34aa
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-13 20:31:28 +0100

    SearchByCategory method => return wikis ids

--- models/Search.php
+++ models/Search.php
@@ -34,4 +34,21 @@ class Search
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    static function searchByCategory($category_name)
+    {
+        global $db;
+        $cat_name_sql = "%" . "$category_name" . "%";
+        $sql = "
+           SELECT w.id
+            FROM wikis w
+                     join categories c on w.id_category = c.id
+            WHERE c.name LIKE :category_name
+              AND status = 'published';
+        ";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':category_name', $cat_name_sql, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
