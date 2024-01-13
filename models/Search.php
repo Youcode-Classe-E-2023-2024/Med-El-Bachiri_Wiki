0fb7e26de45eb8735f5cefd415a3ad26da08a254
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-01-13 20:30:58 +0100

    SearchByTitile method => return wikis ids

--- models/Search.php
+++ models/Search.php
@@ -2,4 +2,18 @@
 
 class Search
 {
+        static function searchByTitle($title)
+        {
+            global $db;
+            $titleSql = '%' . $title . '%';
+            $sql = "
+           SELECT id
+            FROM wikis
+            WHERE title LIKE :title AND status = 'published';
+            ";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':title', $titleSql, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
 }
